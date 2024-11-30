6670d826ee401fd68a23104b521e7f4fe6c94077
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2024-11-30 14:58:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
@@ -21,5 +21,5 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 # sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
 
 #增加其他软件
-sed -i '$a src-git https://github.com/AlexZhuo/luci-app-bandwidthd.git' feeds.conf.default
-sed -i '$a src-git https://github.com/sirpdboy/luci-app-eqosplus' feeds.conf.default
+# sed -i '$a src-git https://github.com/AlexZhuo/luci-app-bandwidthd.git' feeds.conf.default
+# sed -i '$a src-git https://github.com/sirpdboy/luci-app-eqosplus' feeds.conf.default
