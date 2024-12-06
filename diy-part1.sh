5e67ba4e510e5ccf0a5b920c57d71fd068bc3968
lejitao2013 <61481263+lejitao2013@users.noreply.github.com>
2024-12-06 22:34:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,7 @@ sed -i "/helloworld/d" "feeds.conf.default"
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
